5e70c1b682131630c955524b86989ce87daf26a5
Quy Phan <quy.120993@gmail.com>
2023-10-11 19:34:04 +0700

    add arg and try catch args

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
-console.log('tool/bin/index.js');
+const arg = require('arg');
+
+try {
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean,
+  });
+
+  if (args['--start']) {
+    console.log('start');
+  }
+} catch (e) {
+  console.log(e.message);
+  console.log();
+  usage();
+}
+
+function usage() {
+  console.log('Usage: tool [options]');
+  console.log();
+  console.log('Options:');
+  console.log('  --start    start');
+  console.log('  --build    build');
+  console.log();
+}
