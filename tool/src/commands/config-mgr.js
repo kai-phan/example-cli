e44a2969e60b74a5b18740dc1c551d230f77c4d6
Quy Phan <quy.120993@gmail.com>
2023-10-11 21:01:31 +0700

    add ajv for vaidate config and format errors

--- tool/src/commands/config-mgr.js
+++ tool/src/commands/config-mgr.js
@@ -1,5 +1,10 @@
 const chalk = require('chalk');
 const cosmiconfig = require('cosmiconfig');
+const Ajv = require('ajv').default;
+const betterAjvErrors = require('better-ajv-errors').default;
+const schema = require('../config/schema.json');
+
+const ajv = new Ajv();
 
 const explorerSync = cosmiconfig.cosmiconfigSync('tool');
 
@@ -7,6 +12,12 @@ module.exports = function getConfig() {
   const result = explorerSync.search(process.cwd());
 
   if (result?.config) {
+    const validate = ajv.validate(schema, result.config);
+    if (!validate) {
+      console.log(betterAjvErrors(schema, result.config, ajv.errors));
+      process.exit(1);
+    }
+
     return result.config;
   } else {
     console.log(chalk.yellowBright('No config found'));
